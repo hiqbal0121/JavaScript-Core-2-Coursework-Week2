418e14a9ee93ec821975ba4e0d4bff77edf3d2fa
haroon iqbal <h.iqbal0121@gmail.com>
2021-01-23 09:09:47 +0000

    update

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,31 @@
 function todoList(todos) {
   // Write your code here...
+
+  let content = document.getElementById("content");
+  let ul = document.createElement("ul");
+
+  todos.forEach((element) => {
+    let li = document.createElement("li");
+    li.innerHTML = element.todo;
+    li.addEventListener("click", (event) => {
+      if (event.target.style.textDecoration === "line-through") {
+        event.target.style.textDecoration = "none";
+      } else {
+        event.target.style.textDecoration = "line-through";
+      }
+    });
+
+    ul.appendChild(li);
+  });
+
+  content.appendChild(ul);
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
