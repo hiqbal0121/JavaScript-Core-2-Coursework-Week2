e2e29e49174144a24091bd499e9757b625b241e5
haroon iqbal <h.iqbal0121@gmail.com>
2021-01-23 08:41:04 +0000

    exercises

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,20 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
-}
+// function shoppingList(arrayOfPeople) {
+//   // Write your code here...
+// }
 
+function shoppingList(list) {
+  let content = document.querySelector("#content");
+
+  let ul = document.createElement("ul");
+
+  for (let i = 0; i < list.length; i++) {
+    let liList = document.createElement("li");
+    liList.innerHTML = list[i];
+    ul.appendChild(liList);
+  }
+
+  content.appendChild(ul);
+}
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
 shoppingList(shopping);
--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,23 +1,64 @@
 function readingList(books) {
   // Write your code here...
+  let content = document.getElementById("content");
+  let h1 = document.createElement("h1");
+  h1.innerHTML = `Booklist`;
+  content.appendChild(h1);
+
+  let ul = document.createElement("ul");
+  ul.style.listStyleType = "none";
+  ul.style.display = "flex";
+  ul.style.flexWrap = "wrap";
+  ul.style.padding = "30px";
+  ul.style.width = "100%";
+
+  for (let i = 0; i < books.length; i++) {
+    let list = document.createElement("li");
+    list.style.listStyleType = "none";
+    list.style.padding = "10px";
+    list.style.margin = "15px";
+    list.style.width = "25%";
+  }
+
+  if (books[i].alreadyRead === true) {
+    list.style.backgroundColor = "green";
+  } else {
+    list.style.backgroundColor = "red";
+  }
+
+  let bookP = document.createElement("p");
+  bookP.innerHTML = `${books[i].title} - ${books[i].author}`;
+  list.appendChild(bookP);
+
+  let coverImage = document.createElement("img");
+  coverImage.src = books[i].coverImage;
+  coverImage.style.width = "60%";
+
+  list.appendChild(coverImage);
+  ul.appendChild(list);
+  content.appendChild(ul);
 }
 
 const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
+    coverImage: "https://miro.medium.com/max/1200/1*Qo27inBKBKY4Q4Pgk5KkbQ.png",
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
+    coverImage:
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
-  }
+    alreadyRead: true,
+    coverImage: "https://m.media-amazon.com/images/I/51A8l+FxFNL._SL500_.jpg",
+  },
 ];
 
 readingList(books);
