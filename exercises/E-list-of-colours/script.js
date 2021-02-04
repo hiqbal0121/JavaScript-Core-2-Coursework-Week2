a7b9d53ff23f451432069cf68c53bbdd23691731
haroon iqbal <h.iqbal0121@gmail.com>
2021-02-04 08:25:37 +0000

    Update E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,25 @@
 function listOfColours(colours) {
-  // Write your code here...
+  let content = document.getElementById("content");
+
+  let colourList = document.createElement("select");
+  colourList.id = "colours";
+
+  let paragraph = document.createElement("p");
+  paragraph.innerHTML = "This paragraph changes when you select a colour!";
+
+  for (let i = 0; i < colours.length; i++) {
+    let chosenColour = document.createElement("option");
+    chosenColour.innerHTML = colours[i];
+    colourList.appendChild(chosenColour);
+  }
+
+  colourList.addEventListener("change", function () {
+    paragraph.innerHTML = `You have selected ${this.value}!`;
+    paragraph.style.color = this.value;
+  });
+
+  content.appendChild(colourList);
+  content.appendChild(paragraph);
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
